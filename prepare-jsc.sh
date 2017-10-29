f1412aae94f7f7fe60e598624139f90f1a195bf3
Leo Natan <lnatan@wix.com>
2017-10-29 19:37:36 +0200

    Rename custom JavaScriptCore to “DTX_JSC”

--- prepare-jsc.sh
+++ prepare-jsc.sh
@@ -5,7 +5,7 @@ if [ ! -d "wk-svn" ]; then
 	git clone git://git.webkit.org/WebKit.git wk-svn
 fi
 
-rm -fr JSC.framework
+rm -fr DTX_JSC.framework
 
 cd wk-svn
 
@@ -13,9 +13,9 @@ patch -p1 < ../wk.diff
 
 export CODE_SIGNING_REQUIRED=NO && Tools/Scripts/build-webkit --ios-simulator --sdk=iphonesimulator
 export CODE_SIGNING_REQUIRED=NO && Tools/Scripts/build-webkit --ios-device --sdk=iphoneos
-cp -r WebKitBuild/Release-iphoneos/JavaScriptCore.framework ../JSC.framework
-rm -f ../JSC.framework/JavaScriptCore
-lipo -create WebKitBuild/Release-iphoneos/JavaScriptCore.framework/JavaScriptCore WebKitBuild/Release-iphonesimulator/JavaScriptCore.framework/JavaScriptCore -output ../JSC.framework/JSC 
-install_name_tool -id JSC ../JSC.framework/JSC
-/usr/libexec/PlistBuddy -c "Set :CFBundleExecutable JSC" -c "Set :CFBundleName JSC" -c "Set :CFBundleIdentifier com.wix.JavaScriptCore" ../JSC.framework/Info.plist
-codesign -f -s - -- ../JSC.framework
+cp -r WebKitBuild/Release-iphoneos/JavaScriptCore.framework ../DTX_JSC.framework
+rm -f ../DTX_JSC.framework/JavaScriptCore
+lipo -create WebKitBuild/Release-iphoneos/JavaScriptCore.framework/JavaScriptCore WebKitBuild/Release-iphonesimulator/JavaScriptCore.framework/JavaScriptCore -output ../DTX_JSC.framework/DTX_JSC 
+install_name_tool -id DTX_JSC ../DTX_JSC.framework/DTX_JSC
+/usr/libexec/PlistBuddy -c "Set :CFBundleExecutable DTX_JSC" -c "Set :CFBundleName DTX_JSC" -c "Set :CFBundleIdentifier com.wix.JavaScriptCore" ../DTX_JSC.framework/Info.plist
+codesign -f -s - -- ../DTX_JSC.framework
